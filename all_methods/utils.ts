3ec3a7745065c4b595cf960e6b3c01d6f4a620ca
Muktha Ramesh <muktharamesh20@gmail.com>
2025-06-08 02:36:02 -0400

    Created a bunch of types to make everything cleaner

--- all_methods/utils.ts
+++ all_methods/utils.ts
@@ -1,7 +1,32 @@
-export type Post = {image: string, caption: string, categories: {name:string, id: string}[], ownerId: string, id: string, createdAt: Date, likeCount:number
-, likedByMe: boolean, inspiredCount: number, comments: Comment[]};
-export type Comment = {id: string, ownerId: string, postId: string, content: string, createdAt: Date, ownerUsername: string, reply_to_id: string | null};
+export type Post = {image: Image, caption: Text, categories: Category, ownerId: UserId, id: PostId, createdAt: Date, likeCount:number
+, likedByMe: boolean, inspiredCount: number, comments: Comment[], linkedObject: Event | Todo};
+export type Comment = {id: CommentId, ownerId: UserId, postId: PostId, content: Text, createdAt: Date, ownerUsername: UserId, reply_to_id: CommentId | null};
 export type Calendar = {forNow:null}
+export type Group = {forNow: null}
+export type UserHomePage = {id: CategoryId, CategoryName: string, orderNum: number, posts: Post[], tags: Post[], followers: UserList, following: UserList, profile: User};
+export type GroupHomePage = {CategoryName: string, orderNum: number, posts: Post[], tags: Post[], members: UserList, profile: User};
+export type Feed = {forNow: null}
+export type Message = {id: string, content: Text, createdAt: Date, senderId: string}
+export type GroupChat = {name: string, id: string, members: {id: string, username: string}[], messages: Message[]};
+export type FrinedChat = {name: string, id: string, members: {id: string, username: string}[], messages: Message[]};
+export type Todo = {forNow: null}
+export type KanbanBoard = {forNow: null}
+export type Event = {forNow: null, canModify: boolean, canDelete: boolean}
+export type Notification = {forNow: null}
+export type NotificationList = {forNow: null}
+export type User = {forNow: null}
+export type UserList = {forNow: null}
+export type Image = {forNow: null}
+export type UserSettingsPage = {forNow: null}
+export type Category = {name: string, id: string, orderNum: number};
+export type Text = string;
+export type UserId = string;
+export type CommentId = string;
+export type PostId = string;
+export type GroupId = string;
+export type EventId = string;
+export type TodoId = string;
+export type CategoryId = string;
 
 export async function asyncTimer(ms: number): Promise<void> {
     return new Promise(resolve => setTimeout(resolve, ms));
